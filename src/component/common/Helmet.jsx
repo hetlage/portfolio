99322f249e92d8f250ca5e830a97f090c8e75807
johnsriot <gr8appskc@gmail.com>
2020-05-26 00:07:15 -0500

    Added open graph tags statically

--- src/component/common/Helmet.jsx
+++ src/component/common/Helmet.jsx
@@ -4,27 +4,13 @@ import { Helmet } from "react-helmet";
 class PageHelmet extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Helmet>
-          <title>
-            {this.props.pageTitle} || Web Developer, Programmer, Maker &
-            Entrepreneur{" "}
-          </title>
-          <meta
-            name="description"
-            content="Welcome to JohnHetlage.com. This site is dedicated to provide a view into my world."
-          />
-          <meta property="og:url" content="https://www.johnhetlage.com" />
-          <meta
-            property="og:description"
-            content="Welcome to JohnHetlage.com. This site is dedicated to provide a view into my world."
-          />
-          <meta
-            property="og:image"
-            content="/assets/images/matrix-2354492_1280.jpg"
-          />
-        </Helmet>
-      </React.Fragment>
+      <Helmet>
+        {/* General tags */}
+        <title>
+          {this.props.pageTitle} || Web Developer, Programmer, Maker &
+          Entrepreneur{" "}
+        </title>
+      </Helmet>
     );
   }
 }
